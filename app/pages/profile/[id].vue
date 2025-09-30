bb0dd092c70b618bab9864632e32218f42d239b6
Ustinix <ksuletters@yandex.ru>
2025-09-30 11:17:37 +0300

    first commit

---
+++ app/pages/profile/[id].vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+const route = useRoute();
+const routeId = route.params.id;
+const { data, error, execute} = useAsyncData(() => $fetch(`/api/hello?userId=${routeId}`))
+</script>
+<template>
+    <div>
+profile with id - {{ route.params.id }}
+<div>{{data}}</div>
+<div>{{ error }}</div>
+<button @onClick="execute">execute</button>
+    </div>
+</template>
+
+<style scoped>
+
+</style>
